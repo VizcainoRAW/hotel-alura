c487a5c105aef3d31a9ee6601254bb0a4cfb3a59
ivan vizcaino <ivanandres1305@gmail.com>
2024-01-07 23:59:59 -0500

    feat(project): refactor project structure

--- src/main/java/com/vizcainoraw/hotelalura/HotelAluraApplication.java
+++ src/main/java/com/vizcainoraw/hotelalura/HotelAluraApplication.java
@@ -1,4 +1,4 @@
-package com.hotelalura.hotelalura;
+package com.vizcainoraw.hotelalura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/vizcainoraw/hotelalura/HotelAluraApplicationTests.java
+++ src/test/java/com/vizcainoraw/hotelalura/HotelAluraApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hotelalura.hotelalura;
+package com.vizcainoraw.hotelalura;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
